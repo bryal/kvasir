2ea4413f245b4805b719dd7ae7544eae819f8813
bryal <96.bryal@gmail.com>
2018-01-16 14:52:53 +0100

    Less noise from build_core.sh

--- build_core.sh
+++ build_core.sh
@@ -1,5 +1,5 @@
 clang -c -o pcg_c.o src/core/pcg-c-basic/pcg_basic.c
 clang -c -o core_c.o src/core/core.c
-rm libcore.a
+rm -f libcore.a
 ar rcs libcore.a pcg_c.o core_c.o
-rm pcg_c.o core_c.o
+rm -f pcg_c.o core_c.o
