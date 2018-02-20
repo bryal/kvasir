ded132f3a7981e52bb822d9667eb606ed93549f7
bryal <96.bryal@gmail.com>
2018-02-20 13:22:26 +0100

    Use BTreeMap in collections

--- src/lib/collections.rs
+++ src/lib/collections.rs
@@ -1,7 +1,6 @@
 use std::borrow::Borrow;
-use std::collections::hash_map::HashMap;
+use std::collections::BTreeMap;
 use std::fmt::{self, Debug};
-use std::hash::Hash;
 
 // TODO: Consider using BTreeMap, possible perforance increase. Do benchmarks.
 /// A stack of scopes of something. Fast access due to hashmaps, and guaranteed to contain no
@@ -9,35 +8,35 @@ use std::hash::Hash;
 ///
 /// Used for ConstDef:s. Only one constant can be defined for a single name at any given moment.
 #[derive(Clone)]
-pub struct ScopeStack<K, V>(Vec<HashMap<K, V>>);
+pub struct ScopeStack<K, V>(Vec<BTreeMap<K, V>>);
 
-impl<K: Hash + Eq, V> ScopeStack<K, V> {
+impl<K: Ord + Eq, V> ScopeStack<K, V> {
     pub fn new() -> ScopeStack<K, V> {
         ScopeStack(Vec::new())
     }
 
-    pub fn push(&mut self, scope: HashMap<K, V>) {
+    pub fn push(&mut self, scope: BTreeMap<K, V>) {
         if scope.keys().any(|key| self.contains_key(key)) {
             panic!("ScopeStack::push: Key already exists");
         }
         self.0.push(scope);
     }
 
-    pub fn pop(&mut self) -> Option<HashMap<K, V>> {
+    pub fn pop(&mut self) -> Option<BTreeMap<K, V>> {
         self.0.pop()
     }
 
-    pub fn split_off(&mut self, at: usize) -> Vec<HashMap<K, V>> {
+    pub fn split_off(&mut self, at: usize) -> Vec<BTreeMap<K, V>> {
         self.0.split_off(at)
     }
 
-    pub fn extend(&mut self, xs: Vec<HashMap<K, V>>) {
+    pub fn extend(&mut self, xs: Vec<BTreeMap<K, V>>) {
         self.0.extend(xs)
     }
 
     pub fn contains_key<Q: ?Sized>(&self, key: &Q) -> bool
     where
-        Q: Hash + Eq,
+        Q: Ord + Eq,
         K: Borrow<Q>,
     {
         self.0.iter().any(|scope| scope.contains_key(key))
@@ -45,7 +44,7 @@ impl<K: Hash + Eq, V> ScopeStack<K, V> {
 
     pub fn get_height<Q: ?Sized>(&self, key: &Q) -> Option<usize>
     where
-        Q: Hash + Eq,
+        Q: Ord + Eq,
         K: Borrow<Q>,
     {
         for (height, scope) in self.0.iter().enumerate() {
@@ -58,7 +57,7 @@ impl<K: Hash + Eq, V> ScopeStack<K, V> {
 
     pub fn get<Q: ?Sized>(&self, key: &Q) -> Option<&V>
     where
-        Q: Hash + Eq,
+        Q: Ord + Eq,
         K: Borrow<Q>,
     {
         self.get_with_height(key).map(|(v, _)| v)
@@ -66,7 +65,7 @@ impl<K: Hash + Eq, V> ScopeStack<K, V> {
 
     pub fn get_mut<Q: ?Sized>(&mut self, key: &Q) -> Option<&mut V>
     where
-        Q: Hash + Eq,
+        Q: Ord + Eq,
         K: Borrow<Q>,
     {
         self.get_mut_with_height(key).map(|(v, _)| v)
@@ -74,7 +73,7 @@ impl<K: Hash + Eq, V> ScopeStack<K, V> {
 
     pub fn remove<Q: ?Sized>(&mut self, key: &Q) -> Option<V>
     where
-        Q: Hash + Eq,
+        Q: Ord + Eq,
         K: Borrow<Q>,
     {
         for scope in self.0.iter_mut().rev() {
@@ -87,7 +86,7 @@ impl<K: Hash + Eq, V> ScopeStack<K, V> {
 
     pub fn get_with_height<Q: ?Sized>(&self, key: &Q) -> Option<(&V, usize)>
     where
-        Q: Hash + Eq,
+        Q: Ord + Eq,
         K: Borrow<Q>,
     {
         for (height, scope) in self.0.iter().enumerate().rev() {
@@ -100,7 +99,7 @@ impl<K: Hash + Eq, V> ScopeStack<K, V> {
 
     pub fn get_mut_with_height<Q: ?Sized>(&mut self, key: &Q) -> Option<(&mut V, usize)>
     where
-        Q: Hash + Eq,
+        Q: Ord + Eq,
         K: Borrow<Q>,
     {
         for (height, scope) in self.0.iter_mut().enumerate().rev() {
@@ -112,7 +111,7 @@ impl<K: Hash + Eq, V> ScopeStack<K, V> {
     }
 }
 
-impl<K: Debug + Hash + Eq, V: Debug> Debug for ScopeStack<K, V> {
+impl<K: Debug + Ord + Eq, V: Debug> Debug for ScopeStack<K, V> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         Debug::fmt(&self.0, f)
     }
@@ -174,7 +173,9 @@ where
     K: PartialEq<K>,
 {
     pub fn new() -> Self {
-        AddMap { next: Box::into_raw(Box::new(None)) }
+        AddMap {
+            next: Box::into_raw(Box::new(None)),
+        }
     }
 
     /// Get reference to key and value in map associated with `key`
